360a90be0e2c9736276fe080e979d814dfabb3a4
amila-sandaruwan-git <wkruwan97@gmail.com>
2026-07-10 00:22:55 +0530

    Updated: Updated some codes from package json and next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   images: {
     domains: ['i.ytimg.com', 'img.youtube.com'],
+    unoptimized: true,
   },
+  // Remove or comment out: output: 'export',
+  // output: 'export',  // ← COMMENT THIS OUT
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
 }
 
 module.exports = nextConfig
